ff5ae05e99c9819d3eef1eecfe05e4c2bfc6ae16
Jamal Turner <jlsmitty2273@yahoo.com>
2020-02-20 08:56:32 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200220130526) do
+ActiveRecord::Schema.define(version: 20200220135400) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
